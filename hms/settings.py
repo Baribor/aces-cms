c9a03bd17dc3ec0a24cc4a9581a21b5e1dbe9317
Baribor Saturday <saturdaybaribor@gmail.com>
2023-05-14 03:28:49 +0100

    fnfsi

--- hms/settings.py
+++ hms/settings.py
@@ -84,9 +84,11 @@ WSGI_APPLICATION = 'hms.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASE_URL = os.getenv('DATABASE_URL')
+bgf = "postgres://aces:theamazon@localhost:5432/aces-hms"
+
 
 DATABASES = {
-    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
+    'default': dj_database_url.config(default=bgf, conn_max_age=1800)
 }
 
 
